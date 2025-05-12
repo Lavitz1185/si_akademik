339ce4892f9a57006c651922cd464dc1e2576b09
AdiJT <aditaklal@gmail.com>
2025-05-12 19:30:06 +0800

    Mulai

---
+++ src/SIAkademik/SIAkademik.Domain/Abstracts/Entity.cs
@@ -0,0 +1,16 @@
+﻿namespace SIAkademik.Domain.Abstracts;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public int Id { get; set; }
+
+    public override bool Equals(object? obj) => obj is not null && obj is Entity entity && Equals(entity);
+
+    public override int GetHashCode() => Id.GetHashCode();
+
+    public bool Equals(Entity? other) => other is not null && other.GetType() == GetType() && other.Id == Id;
+
+    public static bool operator ==(Entity? left, Entity? right) => left is not null && left.Equals(right);
+
+    public static bool operator !=(Entity? left, Entity? right) => !(left == right);
+}
---
+++ src/SIAkademik/SIAkademik.Domain/Abstracts/IAuditableEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace SIAkademik.Domain.Abstracts;
+
+public interface IAuditableEntity
+{
+    DateTime TanggalDitambahkan { get; set; }
+    DateTime? TanggalDiubah { get; set; }
+}
