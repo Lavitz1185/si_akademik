2eece3bfef22398389d92182034dae87dcfe7178
AdiJT <aditaklal@gmail.com>
2025-09-27 13:37:43 +0800

    Sedikit

---
+++ src/SIAkademik/SIAkademik.Web/Areas/DashboardGuru/Models/PertemuanModels/AbsenVM.cs
@@ -0,0 +1,26 @@
+﻿using SIAkademik.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace SIAkademik.Web.Areas.DashboardGuru.Models.PertemuanModels;
+
+public class AbsenVM
+{
+    public required int IdPertemuan { get; set; }
+
+    public required List<AbsenEntryVM> DaftarAbsen { get; set; }
+}
+
+public class AbsenEntryVM
+{
+    [Display(Name = "Id")]
+    [Required(ErrorMessage = "{0} harus diisi")]
+    public required int IdSiswa { get; set; }
+
+    [Display(Name = "Siswa")]
+    [Required(ErrorMessage = "{0} harus diisi")]
+    public required string NamaSiswa { get; set; }
+
+    [Display(Name = "Absensi")]
+    [Required(ErrorMessage = "{0} harus diisi")]
+    public required Absensi Absensi { get; set; }
+}
