3e055826d0b06fe5fadbfec8854a96c39454788f
putra <fernandputra14@gmail.com>
2025-10-14 18:22:38 +0800

    update responsive navbar (belum selesai)

---
+++ src/SIAkademik/SIAkademik.Web/wwwroot/js/responsive.js
@@ -0,0 +1,21 @@
+﻿const menuBtn = document.getElementById('menu-btn');
+const menuClose = document.getElementById('menu-close');
+const navUl = document.querySelector('nav .navigation ul');
+const submenuParents = document.querySelectorAll('nav .navigation ul li.has-submenu');
+
+// Buka menu
+menuBtn.addEventListener('click', () => {
+    navUl.classList.add('active');
+});
+
+// Tutup menu
+menuClose.addEventListener('click', () => {
+    navUl.classList.remove('active');
+});
+
+// Toggle submenu di mobile
+submenuParents.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
