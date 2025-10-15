421087e831f1787c94a95b7bc06f81a19aa4427e
AdiJT <aditaklal@gmail.com>
2025-10-15 22:30:26 +0800

    Sedikit

--- src/SIAkademik/SIAkademik.Web/Services/PDFGenerator/IPDFGeneratorService.cs
+++ src/SIAkademik/SIAkademik.Web/Services/PDFGenerator/IPDFGeneratorService.cs
@@ -3,4 +3,5 @@
 public interface IPDFGeneratorService
 {
     Task<byte[]> GeneratePDF(string html, string fileName);
+    Task<byte[]> GeneratePDF(string html, string header, string footer, string fileName);
 }
--- src/SIAkademik/SIAkademik.Web/Services/PDFGenerator/PDFGeneratorService.cs
+++ src/SIAkademik/SIAkademik.Web/Services/PDFGenerator/PDFGeneratorService.cs
@@ -1,6 +1,7 @@
-﻿using PuppeteerSharp.Media;
-using PuppeteerSharp;
+﻿using PuppeteerSharp;
+using PuppeteerSharp.Media;
 using Razor.Templating.Core;
+using System.Reflection.PortableExecutable;
 
 namespace SIAkademik.Web.Services.PDFGenerator;
 
@@ -17,13 +18,44 @@ public class PDFGeneratorService : IPDFGeneratorService
 
     public async Task<byte[]> GeneratePDF(string html, string fileName)
     {
-        var header = await _razorTemplateEngine.RenderAsync("Views/Shared/_HeaderLaporanPartial.cshtml");
-        var footer = await _razorTemplateEngine.RenderAsync("Views/Shared/_FooterLaporanPartial.cshtml");
+        var header = await _razorTemplateEngine.RenderAsync("Views/Shared/_Header1LaporanPartial.cshtml");
+        var footer = await _razorTemplateEngine.RenderAsync("Views/Shared/_Footer1LaporanPartial.cshtml");
 
         var browserFetcher = new BrowserFetcher();
         await browserFetcher.DownloadAsync();
 
         using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = true, Args = ["--no-sandbox"] });
+        using var page = await browser.NewPageAsync();
+        await page.SetContentAsync(html);
+
+        if (!Directory.Exists($"{_webHostEnvironment.WebRootPath}/file/pdf"))
+            Directory.CreateDirectory($"{_webHostEnvironment.WebRootPath}/file/pef");
+
+        var filePath = $"{_webHostEnvironment.WebRootPath}/file/pdf/{fileName}.pdf";
+        await page.PdfAsync(filePath, new PdfOptions
+        {
+            DisplayHeaderFooter = true,
+            HeaderTemplate = header,
+            FooterTemplate = footer,
+            Format = PaperFormat.A4,
+            MarginOptions = new MarginOptions
+            {
+                Bottom = "50px",
+                Top = "170px",
+                Left = "80px",
+                Right = "80px",
+            }
+        });
+
+        return await File.ReadAllBytesAsync(filePath);
+    }
+
+    public async Task<byte[]> GeneratePDF(string html, string header, string footer, string fileName)
+    {
+        var browserFetcher = new BrowserFetcher();
+        await browserFetcher.DownloadAsync();
+
+        using var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Timeout = 0, Headless = true, Args = ["--no-sandbox"] });
         using var page = await browser.NewPageAsync();
         await page.SetContentAsync(html);
 
