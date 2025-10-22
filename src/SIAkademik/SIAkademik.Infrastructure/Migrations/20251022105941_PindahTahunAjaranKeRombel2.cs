8d73581627e28ebcc615625d5660d18e863bc2b6
AdiJT <aditaklal@gmail.com>
2025-10-22 20:12:45 +0800

    Sedikit

---
+++ src/SIAkademik/SIAkademik.Infrastructure/Migrations/20251022105941_PindahTahunAjaranKeRombel2.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SIAkademik.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class PindahTahunAjaranKeRombel2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TblKelas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PeminatanId",
+                value: 5);
+
+            migrationBuilder.UpdateData(
+                table: "TblKelas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PeminatanId",
+                value: 1);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "TblKelas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "PeminatanId",
+                value: 1);
+
+            migrationBuilder.UpdateData(
+                table: "TblKelas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "PeminatanId",
+                value: 5);
+        }
+    }
+}
---
+++ src/SIAkademik/SIAkademik.Web/Areas/DashboardAdmin/Models/RombelModels/DetailVM.cs
@@ -0,0 +1,19 @@
+﻿using SIAkademik.Domain.ModulSiakad.Entities;
+
+namespace SIAkademik.Web.Areas.DashboardAdmin.Models.RombelModels;
+
+public class DetailVM
+{
+    public required int Id { get; set; }
+    public Rombel Rombel { get; set; }
+
+    public required List<DetailEntryVM> DaftarSiswaTambah { get; set; }
+    public required List<DetailEntryVM> DaftarSiswaHapus { get; set; }
+}
+
+public class DetailEntryVM
+{
+    public required Siswa Siswa { get; set; }
+    public required int IdSiswa { get; set; }
+    public bool Selected { get; set; }
+}
