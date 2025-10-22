40d45089ac04812e4a716031106375dad1447710
AdiJT <aditaklal@gmail.com>
2025-10-22 22:10:52 +0800

    Sedikit

--- src/SIAkademik/SIAkademik.Web/Areas/DashboardAdmin/Models/SiswaModels/IndexVM.cs
+++ src/SIAkademik/SIAkademik.Web/Areas/DashboardAdmin/Models/SiswaModels/IndexVM.cs
@@ -1,15 +1,17 @@
 ﻿using SIAkademik.Domain.Enums;
 using SIAkademik.Domain.ModulSiakad.Entities;
+using SIAkademik.Web.Models;
 
 namespace SIAkademik.Web.Areas.DashboardAdmin.Models.SiswaModels;
 
 public class IndexVM
 {
-    public JenisKelamin? JenisKelamin { get; set; }
-    public Agama? Agama { get; set; }
-    public StatusAktifMahasiswa? StatusAktif { get; set; }
-    public DateOnly? TanggalMasuk { get; set; }
-    public Jenjang? Jenjang { get; set; }
+    public List<FilterEntryVM<JenisKelamin>> JenisKelamin { get; set; } = [];
+    public List<FilterEntryVM<Agama>> Agama { get; set; } = [];
+    public List<FilterEntryVM<StatusAktifMahasiswa>> StatusAktif { get; set; } = [];
+    public List<FilterEntryVM<int>> TahunMasuk { get; set; } = [];
+    public List<FilterEntryVM<int>> TahunLahir { get; set; } = [];
+    public List<FilterEntryVM<Jenjang>> Jenjang { get; set; } = [];
 
     public List<Siswa> DaftarSiswa { get; set; } = [];
 }
