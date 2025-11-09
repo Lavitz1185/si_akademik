8c5a2d9920d6701f4e2608c025ec60c3fab8e306
putra <fernandputra14@gmail.com>
2025-11-09 11:21:15 +0800

    Update UI Raport

--- src/SIAkademik/SIAkademik.Web/Areas/DashboardGuru/Controllers/RaportController.cs
+++ src/SIAkademik/SIAkademik.Web/Areas/DashboardGuru/Controllers/RaportController.cs
@@ -461,10 +461,10 @@ public class RaportController : Controller
             header, 
             footer, 
             fileName,
-            marginTop:120,
-            marginBottom:50,
-            marginLeft:80,
-            marginRight:80);
+            marginTop:195,
+            marginBottom:80,
+            marginLeft:75,
+            marginRight:54);
 
         if (download)
             return File(pdfBinary, "application/pdf", fileDownloadName: fileName);
